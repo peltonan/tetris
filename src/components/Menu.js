a40bed0cc3ec6c113980bdebe2e0aef65dd10d01
peltonan <peltonen.annette@gmail.com>
2022-05-18 10:52:18 +0300

    Media rules and added Game Title

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,18 @@
 import "./Menu.css";
 
-<div>
-    <h1 className="GameTitle">TETRIS</h1>
-</div>
+
 const Menu = ({ onClick }) => {
     return (
-        <div className="Menu">
-            <button className="Button" onClick={onClick} >
-                Play
-            </button>
-        </div>)
+        <>
+            <div>
+                <h1 className="GameTitle">TETRIS</h1>
+            </div>
+            <div className="Menu">
+                <button className="Button" onClick={onClick} >
+                    Play
+                </button>
+            </div>
+        </>)
 
 
 }
